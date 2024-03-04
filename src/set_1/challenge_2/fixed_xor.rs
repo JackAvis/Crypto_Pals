60f4770feed42ac0c0219055da78ad5741839ca2
Jackson Davis <jdavis19@ad.unc.edu>
2024-03-03 23:22:46 -0800

    Update fixed_xor.rs

--- src/set_1/challenge_2/fixed_xor.rs
+++ src/set_1/challenge_2/fixed_xor.rs
@@ -2,12 +2,11 @@ use crate::set_1::challenge_1::hex_to_base64::hex_to_base64;
 use hex;
 use std::{cmp::min, ops::BitXor};
 
-pub fn fixed_xor(input: &String, xor_input  : &String) -> String{
+pub fn fixed_xor(input: &String, xor_input: &String) -> String{
     let input_bytes: Vec<u8> = hex::decode(input).unwrap();
     let xor_input_bytes: Vec<u8> = hex::decode(xor_input).unwrap();
     let mut xor_value: Vec<u8> = Vec::new();
     let min_length = min(input_bytes.len(), xor_input_bytes.len());
-    // xor = 1 when only 1 of the bits is 1
     for i in 0..min_length {
         let input_byte: u8 = *input_bytes.get(i).unwrap();
         let xor_input_byte: u8 = *xor_input_bytes.get(i).unwrap();
@@ -20,7 +19,6 @@ pub fn fixed_xor(input: &String, xor_input  : &String) -> String{
 pub fn fixed_xor_bytes(input_bytes: &Vec<u8>, xor_input_bytes: &Vec<u8>) -> Vec<u8>{
     let mut xor_value: Vec<u8> = Vec::new();
     let min_length = min(input_bytes.len(), xor_input_bytes.len());
-    // xor = 1 when only 1 of the bits is 1
     for i in 0..min_length {
         let input_byte: u8 = *input_bytes.get(i).unwrap();
         let xor_input_byte: u8 = *xor_input_bytes.get(i).unwrap();
