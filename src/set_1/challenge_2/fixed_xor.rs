50099567d2fcdc29f5c6642c27df15f4ee769455
Jackson Davis <jdavis19@ad.unc.edu>
2024-03-03 23:22:05 -0800

    Update fixed_xor.rs

--- src/set_1/challenge_2/fixed_xor.rs
+++ src/set_1/challenge_2/fixed_xor.rs
@@ -9,10 +9,10 @@ pub fn fixed_xor(input: &String, xor_input  : &String) -> String{
     let min_length = min(input_bytes.len(), xor_input_bytes.len());
     // xor = 1 when only 1 of the bits is 1
     for i in 0..min_length {
-        let input_octet: u8 = *input_bytes.get(i).unwrap();
-        let xor_input_octet: u8 = *xor_input_bytes.get(i).unwrap();
-        let xor_octet = input_octet ^ xor_input_octet;
-        xor_value.push(xor_octet);
+        let input_byte: u8 = *input_bytes.get(i).unwrap();
+        let xor_input_byte: u8 = *xor_input_bytes.get(i).unwrap();
+        let xor_byte = input_byte ^ xor_input_byte;
+        xor_value.push(xor_byte);
     }
     return hex::encode(xor_value);
 }
@@ -22,10 +22,10 @@ pub fn fixed_xor_bytes(input_bytes: &Vec<u8>, xor_input_bytes: &Vec<u8>) -> Vec<
     let min_length = min(input_bytes.len(), xor_input_bytes.len());
     // xor = 1 when only 1 of the bits is 1
     for i in 0..min_length {
-        let input_octet: u8 = *input_bytes.get(i).unwrap();
-        let xor_input_octet: u8 = *xor_input_bytes.get(i).unwrap();
-        let xor_octet = input_octet ^ xor_input_octet;
-        xor_value.push(xor_octet);
+        let input_byte: u8 = *input_bytes.get(i).unwrap();
+        let xor_input_byte: u8 = *xor_input_bytes.get(i).unwrap();
+        let xor_byte = input_byte ^ xor_input_byte;
+        xor_value.push(xor_byte);
     }
     return xor_value;
 }
