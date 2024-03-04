5de2000595fc0633e8eecd9b3350d3018bdfebbf
Jackson Davis <jdavis3695@gmail.com>
2024-03-04 10:08:18 -0800

    Solve challenge 5

--- src/set_1/challenge_5/repeating_key_xor.rs
+++ src/set_1/challenge_5/repeating_key_xor.rs
@@ -1,5 +1,15 @@
 
 
 pub fn repeating_key_xor(input: &String, key: &String) -> String {
-    return String::from("output");
+    let input_bytes = input.as_bytes();
+    let key_bytes = key.as_bytes();
+    let mut key_index = 0;
+    let mut encrypted_input = Vec::new();
+    let mut i = 0;
+    for byte in input_bytes.iter(){
+        key_index = i % key_bytes.len();
+        i += 1;
+        encrypted_input.push(byte ^ key_bytes[key_index]);
+    }
+    return hex::encode(&encrypted_input);
 }
